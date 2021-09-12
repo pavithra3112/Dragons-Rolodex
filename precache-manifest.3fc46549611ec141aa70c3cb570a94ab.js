b98f6fb7b8369eb3bb096b417fd0b0721d4fe844
Selvakumar <pavithra.selvakumar@accenture.com>
2021-09-12 18:48:06 +0530

    Updates

---
+++ precache-manifest.3fc46549611ec141aa70c3cb570a94ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134669481aee4a27a12c52b45dbf95cd",
+    "url": "/pavithra3112/Dragons-Rolodex.git/index.html"
+  },
+  {
+    "revision": "40248f2418e10dd14b48",
+    "url": "/pavithra3112/Dragons-Rolodex.git/static/css/main.0277af98.chunk.css"
+  },
+  {
+    "revision": "0c2773c1d3b422df3410",
+    "url": "/pavithra3112/Dragons-Rolodex.git/static/js/2.27698087.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pavithra3112/Dragons-Rolodex.git/static/js/2.27698087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40248f2418e10dd14b48",
+    "url": "/pavithra3112/Dragons-Rolodex.git/static/js/main.286dc56a.chunk.js"
+  },
+  {
+    "revision": "f2ea4dfb8df33081d7ba",
+    "url": "/pavithra3112/Dragons-Rolodex.git/static/js/runtime-main.f00a744c.js"
+  }
+]);
